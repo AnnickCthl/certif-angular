8206f9b6764bd9ef34bad4c3881489aef25b5337
ancathala <annick.cathala2@soprasteria.com>
2023-08-11 09:03:28 +0200

    try update

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
     <h1>Hello from {{name}}!</h1>
     <a target="_blank" href="https://angular.io/start">
       Learn more about Angular 
+      And modify it
     </a>
   `,
 })
