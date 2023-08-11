d79a28ac0cd46fb16eed4254d8548dd818d6b642
AnnickCthl <45784382+AnnickCthl@users.noreply.github.com>
2023-08-11 16:28:49 +0200

    2 files modified

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -3,6 +3,9 @@ import { Injectable } from '@angular/core';
 import { map, Observable, of } from 'rxjs';
 import { Category } from '../models/category';
 
+export type GenericObject<T> = { [key: string]: T };
+export type GenericAny<T> = T | T[] | GenericObject<T>;
+
 @Injectable({
   providedIn: 'root',
 })
@@ -13,39 +16,19 @@ export class DataService {
 
   public getCategory(): Observable<Category[]> {
     return this._httpClient
-      .get<Object>(`${this.CATEGORY_URL}`) // TODO Enveler "any"
+      .get<GenericObject<Category[]>>(`${this.CATEGORY_URL}`) // TODO Enveler "any"
       .pipe(
         map((obj) => {
-          
-          console.log(obj);
-          // obj.
           const prop: string[] = Object.getOwnPropertyNames(obj);
 
           if (prop.length === 1) {
-            console.log('visdrdgmjo');
+            let categories = obj[prop[0]]; // TODO
 
-            //   this._mapCategory(obj['trivia_categories']);
-            // pouet = obj.trivia_categories as Category[]; // TODO
-            let pouet = obj[prop[0]]; // TODO
+            return categories;
           }
-          console.log('viscsqvcsdrdgmjo');
-          // return pouet;
+
           return [];
         })
       );
   }
-  // private _mapCategory(obj): Category[] {
-  //   console.log(obj);
-  //   return [];
-  // }
-
-  // private _mapAisBase(src: AisBase[]): AisBase[] {
-  //   const clones: AisBase[] = [];
-
-  //   CloneUtils.defaultArray<AisBase>(src).forEach((value: AisBase) => {
-  //     clones.push(new AisBase(value));
-  //   });
-
-  //   return clones;
-  // }
 }
