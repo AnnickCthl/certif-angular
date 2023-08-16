711d33c4d110e7c36ad782adadd35b552a6ad3c5
AnnickCthl <45784382+AnnickCthl@users.noreply.github.com>
2023-08-16 20:38:04 +0200

    4 files modified

--- src/app/components/five-questions-quiz/five-questions-quiz.component.ts
+++ src/app/components/five-questions-quiz/five-questions-quiz.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Quiz } from '../../models/quiz';
@@ -8,16 +8,16 @@ import { Quiz } from '../../models/quiz';
   templateUrl: './five-questions-quiz.component.html',
   styleUrls: ['./five-questions-quiz.component.css'],
 })
-export class FiveQuestionsQuizComponent implements OnInit {
-  @Input() public fiveQuestionQuiz: Quiz[] = [];
+export class FiveQuestionsQuizComponent {
+  @Input()
+  public get fiveQuestionQuiz(): Quiz[] {
+    return this._fiveQuestionQuiz;
+  }
+  public set fiveQuestionQuiz(value: Quiz[]) {
+    if (value) {
+      this._fiveQuestionQuiz = value;
 
-  public formQuiz: FormGroup = new FormGroup({});
-  public backgroundButtonColor: string = '';
-
-  public constructor(private _router: Router) {}
-
-  public ngOnInit() {
-    if (this.fiveQuestionQuiz) {
+      this.formQuiz = new FormGroup({});
       this.fiveQuestionQuiz.forEach((quiz, index) => {
         this.formQuiz.addControl(
           index.toString(),
@@ -26,6 +26,13 @@ export class FiveQuestionsQuizComponent implements OnInit {
       });
     }
   }
+
+  public formQuiz: FormGroup = new FormGroup({});
+  public backgroundButtonColor: string = '';
+
+  private _fiveQuestionQuiz: Quiz[] = [];
+
+  public constructor(private _router: Router) {}
 
   public onClickSubmit(formQuiz: FormGroup) {
     this._router.navigateByUrl('/results', {
